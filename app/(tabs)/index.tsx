aa1e91083f1ebd4a86e072e05a7f231aac42b33a
rhyg <rhysgeary@gmail.com>
2024-05-13 20:58:32 +1000

    Project setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+const markers = [
+  { name: "Navala Churrascaria", latLng: { latitude: -27.4669946, longitude: 153.0310738 } },
+  { name: "Vizzeto Woodfire", latLng: { latitude: -27.535453, longitude: 153.0326009 } },
+  { name: "Broken Hearts Burger Club", latLng: { latitude: -27.4858021, longitude: 153.0011157 } },
+];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <MapView style={styles.map}>
+        {markers.map((marker, index) => (
+          <Marker key={`${marker.name}-${index}`} coordinate={marker.latLng} title={marker.name} />
+        ))}
+      </MapView>
     </View>
   );
 }
@@ -16,16 +24,20 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
