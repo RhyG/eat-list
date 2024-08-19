4251dbc8af8d4cea15cedd36289ca07dfbb76581
rhyg <rhysgeary@gmail.com>
2024-08-20 06:35:29 +1000

    Places constant

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,20 +3,13 @@ import MapView, { Marker } from "react-native-maps";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
-const markers = [
-  { name: "Navala Churrascaria", latLng: { latitude: -27.4669946, longitude: 153.0310738 } },
-  { name: "Vizzeto Woodfire", latLng: { latitude: -27.535453, longitude: 153.0326009 } },
-  { name: "Broken Hearts Burger Club", latLng: { latitude: -27.4858021, longitude: 153.0011157 } },
-  { name: "Hanwoori", latLng: { latitude: -27.472049, longitude: 153.026819 } },
-  { name: "Sankalp Indian", latLng: { latitude: -27.5122047, longitude: 153.0312011 } },
-  { name: "Sovereign Tavern", latLng: { latitude: -20.0771255, longitude: 146.2632904 } },
-];
+import { places } from "@/constants/places";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <MapView style={styles.map}>
-        {markers.map((marker, index) => (
+        {places.map((marker, index) => (
           <Marker key={`${marker.name}-${index}`} coordinate={marker.latLng} title={marker.name} />
         ))}
         <OverlayComponent />
---
+++ constants/places.ts
@@ -0,0 +1,8 @@
+export const places = [
+  { name: "Navala Churrascaria", latLng: { latitude: -27.4669946, longitude: 153.0310738 } },
+  { name: "Vizzeto Woodfire", latLng: { latitude: -27.535453, longitude: 153.0326009 } },
+  { name: "Broken Hearts Burger Club", latLng: { latitude: -27.4858021, longitude: 153.0011157 } },
+  { name: "Hanwoori", latLng: { latitude: -27.472049, longitude: 153.026819 } },
+  { name: "Sankalp Indian", latLng: { latitude: -27.5122047, longitude: 153.0312011 } },
+  { name: "Sovereign Tavern", latLng: { latitude: -20.0771255, longitude: 146.2632904 } },
+];
