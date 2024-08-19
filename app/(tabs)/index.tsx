c1b16b3bc99066fb22f09bfaf4fc1846c1574591
rhyg <rhysgeary@gmail.com>
2024-08-20 06:24:42 +1000

    Added more example restaurants.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,9 @@ const markers = [
   { name: "Navala Churrascaria", latLng: { latitude: -27.4669946, longitude: 153.0310738 } },
   { name: "Vizzeto Woodfire", latLng: { latitude: -27.535453, longitude: 153.0326009 } },
   { name: "Broken Hearts Burger Club", latLng: { latitude: -27.4858021, longitude: 153.0011157 } },
+  { name: "Hanwoori", latLng: { latitude: -27.472049, longitude: 153.026819 } },
+  { name: "Sankalp Indian", latLng: { latitude: -27.5122047, longitude: 153.0312011 } },
+  { name: "Sovereign Tavern", latLng: { latitude: -20.0771255, longitude: 146.2632904 } },
 ];
 
 export default function TabOneScreen() {
