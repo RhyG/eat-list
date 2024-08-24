efb9c926128d820580cffe52842e4cf8c9c93a98
rhyg <rhysgeary@gmail.com>
2024-08-24 12:10:21 +1000

    Even prettier list screen.

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -4,9 +4,19 @@ import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
 import { places } from "@/constants/places";
-import { Link } from "expo-router";
+import { Link, useNavigation } from "expo-router";
+import { useLayoutEffect } from "react";
 
 export default function TabTwoScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      headerTransparent: true,
+    });
+  });
+
   const renderItem: ListRenderItem<(typeof places)[number]> = ({ item }) => {
     return (
       <Link
@@ -16,17 +26,19 @@ export default function TabTwoScreen() {
         }}
       >
         <View style={styles.item}>
-          <View style={{ gap: 5 }}>
+          <View style={{ gap: 3 }}>
             <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Text style={{ fontWeight: "bold", fontSize: 18 }}>{item.name}</Text>
-              <Rating rating={item.rating} />
+              <Text style={{ fontWeight: "bold", fontSize: 16 }}>{item.name}</Text>
             </View>
             <View style={{ flexDirection: "row", alignItems: "center" }}>
               <Feather name="map-pin" size={14} color="#626268" />
-              <Text style={{ color: "#626268", marginLeft: 5 }}>{item.address}</Text>
+              <Text style={{ color: "#626268", marginLeft: 5, fontSize: 12 }}>{item.address}</Text>
             </View>
           </View>
-          <View>{item.visited ? <Feather name="check-circle" size={22} color="#22c55e" /> : null}</View>
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+            <View>{item.visited ? <Rating rating={item.rating} /> : null}</View>
+            <View>{item.visited ? <Feather name="check-circle" size={22} color="#22c55e" /> : null}</View>
+          </View>
         </View>
       </Link>
     );
@@ -34,7 +46,8 @@ export default function TabTwoScreen() {
 
   return (
     <View style={styles.container}>
-      <FlatList data={places} renderItem={renderItem} />
+      <Text style={{ fontWeight: "bold", fontSize: 22 }}>All Places</Text>
+      <FlatList data={places} renderItem={renderItem} contentContainerStyle={{ gap: 8, paddingTop: 10 }} />
     </View>
   );
 }
@@ -53,7 +66,7 @@ function Rating({ rating }: { rating: number }) {
       }}
     >
       <Feather name="star" size={14} color="#eab208" />
-      <Text style={{ color: "#eab208", marginLeft: 5, fontWeight: "bold" }}>{rating}</Text>
+      <Text style={{ color: "#eab208", marginLeft: 5, fontWeight: "bold", fontSize: 12 }}>{rating}</Text>
     </View>
   );
 }
@@ -61,14 +74,17 @@ function Rating({ rating }: { rating: number }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 10,
+    paddingTop: 80,
+    backgroundColor: "#f4f4f6",
+    paddingHorizontal: 10,
   },
   item: {
-    paddingHorizontal: 20,
+    paddingHorizontal: 10,
     paddingVertical: 10,
     flexDirection: "row",
     justifyContent: "space-between",
     width: "100%",
     alignItems: "center",
+    borderRadius: 8,
   },
 });
