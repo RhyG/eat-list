2c364ce7d7d5bb734f58dd1b8de35e29a34e3c33
rhyg <rhysgeary@gmail.com>
2024-08-26 13:01:14 +1000

    Removed unused screen.

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface Place {
+  id: string;
+  name: string;
+  visited: boolean;
+  rating: number | null;
+  address: string;
+  latLng: {
+    latitude: number;
+    longitude: number;
+  };
+}
